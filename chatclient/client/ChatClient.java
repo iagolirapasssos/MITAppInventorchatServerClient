7df8ce160fb17f189b837a8134370532edf95e34
iagolirapassos <iagolirapassos@gmail.com>
2023-11-05 16:37:24 -0300

    License added

--- chatclient/client/ChatClient.java
+++ chatclient/client/ChatClient.java
@@ -1,3 +1,33 @@
+/*
+ * The MIT License (MIT)
+Copyright © 2023 <copyright holders>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the “Software”), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, modify, merge, 
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
+to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies 
+or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * ChatClient
+ * This extension creates a chat client
+ * Version: 1.0
+ * Author: Francisco Iago Lira Passos
+ * Date: 2023-10-15
+ * Docs: https://docs.google.com/document/d/1xk9dMfczvjbbwD-wMsr-ffqkTlE3ga0ocCE1KOb2wvw/pub#h.4jyv4s6bnjrd
+ */
+
 package io.chatclient;
 
 import com.google.appinventor.components.annotations.*;
